f0d17fcc6ee9e3d496fb4c001fbf5064f4308e24
Nima Mohajeri <nima.mohajeri0@gmail.com>
2024-02-02 17:27:58 -0800

    Style: Updated homepage description again

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@
 
 export const SITE_TITLE = "Nima's Blog";
 export const SITE_DESCRIPTION =
-  "Welcome to my blog! I write my journey in Interactive & Multimedia Learning. I am a computer science student at UVIC and I like to play volleyball!";
+  "Welcome to my blog! I write my journey in Interactive & Multimedia Learning. I am a computer science student at UVIC and I like to play volleyball!\n I am looking forward in learning about how to share information/teach in a more interactive way that is conducive to people's online learning.";
 export const TWITTER_HANDLE = "@OhNoNima";
 export const MY_NAME = "Nima Mohajeri";
 
